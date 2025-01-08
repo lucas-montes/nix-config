fa5b1339fdbf174b56a2dda91f121639e928e363
lucas <lluc23@hotmail.com>
2025-01-08 16:10:19 +0100

    add home-manager pkg

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,7 @@ virtualisation.docker.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  home-manager
 gzip
 brave
 curl
