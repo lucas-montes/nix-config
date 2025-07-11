2f906880d629ffa560107a2ce3652ac77fc3eae3
Lucas <lluc23@hotmail.com>
2025-07-11 13:53:57 +0200

    add greetd and hyprland cache

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,7 +1,22 @@
 {
+  pkgs,
+  user,
+  ...
+}: {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/hyprland";
+        inherit user;
+      };
+      default_session = initial_session;
+    };
   };
 
   security.pam.services.hyprlock = {};
--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -12,6 +12,7 @@ nix = {
         "https://cachix.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
         "https://devenv.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -20,6 +21,7 @@ nix = {
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
       experimental-features = ["nix-command" "flakes"];
       warn-dirty = false;
